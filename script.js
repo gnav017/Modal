b8a3d79360827417bb763e96092acd9153dea5d6
Gev Navasardyan <gnav017@gmail.com>
2022-02-20 17:00:11 -0800

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+var openBtn = document.querySelector('#open');
+var closeBtn = document.querySelector('#close');
+
+function openModal() {
+    openBtn.addEventListener('click', function() {
+        alert('hello');
+    });
+}
