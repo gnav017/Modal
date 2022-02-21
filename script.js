72d285246dfe83e84cae9d6923e338c52f2d1492
gnav017 <99564188+gnav017@users.noreply.github.com>
2022-02-20 17:02:47 -0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var openBtn = document.querySelector('#open');
-var closeBtn = document.querySelector('#close');
-
-function openModal() {
-    openBtn.addEventListener('click', function() {
-        alert('hello');
-    });
-}
